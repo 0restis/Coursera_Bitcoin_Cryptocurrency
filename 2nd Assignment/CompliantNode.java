57412b8600338e2e011bf024450b446b6696936f
0restis <oretzortz@gmail.com>
2017-01-31 23:47:24 +0100

    Add files via upload

---
+++ 2nd Assignment/CompliantNode.java
@@ -0,0 +1,64 @@
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashSet;
+import static java.util.stream.Collectors.toSet;
+
+/* CompliantNode refers to a node that follows the rules (not malicious)*/
+public class CompliantNode implements Node {
+
+    double p_graph;
+    double p_malicious;
+    double p_txDistribution;
+    int numRounds;
+
+    boolean[] followees;
+    boolean[] malicious;
+    Set<Transaction> pendingTransactions;
+
+    public CompliantNode(double p_graph, double p_malicious, double p_txDistribution, int numRounds) {
+        this.p_graph = p_graph;
+        this.p_malicious = p_malicious;
+        this.p_txDistribution = p_txDistribution;
+        this.numRounds = numRounds;
+    }
+
+    public void setFollowees(boolean[] followees) {
+        this.followees = followees;
+        this.malicious = new boolean[followees.length];    
+    }
+
+    public void setPendingTransaction(Set<Transaction> pendingTransactions) {
+        this.pendingTransactions = pendingTransactions; 
+    }
+
+    public Set<Transaction> sendToFollowers() {
+        Set<Transaction> sendTransactions = new HashSet<>(pendingTransactions);
+        // clear ahead of next round
+        pendingTransactions.clear();
+        return sendTransactions; 
+    }
+
+    public void receiveFromFollowees(Set<Candidate> candidates) {
+        Set<Integer> senders = candidates.stream().map(candidate -> candidate.sender).collect(toSet());
+        for (int i = 0; i < followees.length; i++) {
+            if ( followees[i] && !senders.contains(i))
+                malicious[i] = true;
+        }
+        /*boolean isPresent;
+        for (int i = 0; i < followees.length; i++) {
+            isPresent = false;
+            for (Candidate candidate : candidates){
+                if (candidate.sender == i)
+                    isPresent = true;
+            }
+            if ( followees[i] && !isPresent) {
+                malicious[i] = true;
+            }
+        }*/
+        for (Candidate candidate : candidates) { 
+            if (!malicious[candidate.sender]) {
+                pendingTransactions.add(candidate.tx);
+            }
+        }
+    }
+}

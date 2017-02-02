86bef1cacd35b327d1acef985c3f02a7f256bd86
Orestis Tzortzopoulos <oretzortz@gmail.com>
2017-02-02 01:58:34 +0100

    Add files via upload

---
+++ 3rd Assignment/BlockChain.java
@@ -0,0 +1,133 @@
+// Block Chain should maintain only limited block nodes to satisfy the functions
+// You should not have all the blocks added to the block chain in memory 
+// as it would cause a memory overflow.
+
+// Credit goes to user msilb.
+
+import java.util.HashMap;
+import java.util.ArrayList;
+
+public class BlockChain {
+    public static final int CUT_OFF_AGE = 10;
+
+    private class BlockNode {
+    	public Block block;
+    	public BlockNode parent;
+    	public ArrayList<BlockNode> children;
+    	public int height;
+    	// for making a new block on top of this block
+    	private UTXOPool upool;
+    	
+    	public BlockNode(Block block, BlockNode parent, UTXOPool upool){
+    		this.block = block;
+    		this.parent = parent;
+    		children = new ArrayList<BlockChain.BlockNode>();
+    		this.upool = upool;
+	    	if (parent != null) {
+	    		height = parent.height + 1;
+	    		parent.children.add(this);
+	    	}
+	    	else {
+	    		height = 1;
+	    	}
+    	}
+
+    	public UTXOPool getUTXOPoolCopy() {
+    		return new UTXOPool(upool);
+    	}
+    }
+
+    private BlockNode maxHeightNode;
+    private TransactionPool txPool;
+    private HashMap<ByteArrayWrapper, BlockNode> blockChain;
+    
+    /**
+     * create an empty block chain with just a genesis block. Assume {@code genesisBlock} is a valid
+     * block
+     */
+    public BlockChain(Block genesisBlock) {
+        blockChain = new HashMap<>();
+        UTXOPool utxopool = new UTXOPool();
+        addCoinbaseToUTXOPool(genesisBlock, utxopool);
+        BlockNode genesisNode = new BlockNode(genesisBlock, null, utxopool); 
+        blockChain.put(wrap(genesisBlock.getHash()), genesisNode);     
+        txPool = new TransactionPool();
+        maxHeightNode = genesisNode;
+    }
+
+    /**
+     * Get the maximum height block
+     */
+    public Block getMaxHeightBlock() {
+        return maxHeightNode.block;
+    }
+
+    /** Get the UTXOPool for mining a new block on top of max height block */
+    public UTXOPool getMaxHeightUTXOPool() {
+        return maxHeightNode.getUTXOPoolCopy();
+    }
+    
+    /** Get the transaction pool to mine a new block */
+    public TransactionPool getTransactionPool() {
+        return txPool;
+    }
+
+    /**
+     * Add {@code block} to the block chain if it is valid. For validity, all transactions should be
+     * valid and block should be at {@code height > (maxHeight - CUT_OFF_AGE)}.
+     * 
+     * <p>
+     * For example, you can try creating a new block over the genesis block (block height 2) if the
+     * block chain height is {@code <=
+     * CUT_OFF_AGE + 1}. As soon as {@code height > CUT_OFF_AGE + 1}, you cannot create a new block
+     * at height 2.
+     * 
+     * @return true if block is successfully added
+     */
+    public boolean addBlock(Block block) {
+        byte[] prevBlockHash = block.getPrevBlockHash();
+        if (prevBlockHash == null) {
+            return false;
+        }
+        BlockNode parentBlockNode = blockChain.get(wrap(prevBlockHash));
+        if (parentBlockNode == null) {
+            return false;
+        }
+        TxHandler handler = new TxHandler(parentBlockNode.getUTXOPoolCopy());
+        Transaction[] txs = block.getTransactions().toArray(new Transaction[0]);
+        Transaction[] validTxs = handler.handleTxs(txs);
+        if (validTxs.length != txs.length) {
+            return false;
+        }
+        int proposedHeight = parentBlockNode.height + 1;
+        if (proposedHeight <= maxHeightNode.height - CUT_OFF_AGE) {
+            return false;
+        }
+        UTXOPool utxoPool = handler.getUTXOPool();
+        addCoinbaseToUTXOPool(block, utxoPool);
+        BlockNode node = new BlockNode(block, parentBlockNode, utxoPool);
+        blockChain.put(wrap(block.getHash()), node);
+        if (proposedHeight > maxHeightNode.height) {
+            maxHeightNode = node;
+        }
+        return true;
+    }
+
+    /** Add a transaction to the transaction pool */
+    public void addTransaction(Transaction tx) {
+        txPool.addTransaction(tx);
+    }
+
+    public void addCoinbaseToUTXOPool(Block block, UTXOPool utxoPool) {
+        Transaction coinbase = block.getCoinbase();
+        for (int i = 0; i < coinbase.numOutputs(); i++) {
+            Transaction.Output out = coinbase.getOutput(i);
+            UTXO utxo = new UTXO(coinbase.getHash(), i);
+            utxoPool.addUTXO(utxo, out);
+        }
+    }
+
+    private static ByteArrayWrapper wrap(byte[] arr) {
+        return new ByteArrayWrapper(arr);
+    }
+}
